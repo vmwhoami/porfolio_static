c7ebbe576546326681af2c49877cad760ee24803
Vitalie Melnic <vmwhoami@gmail.com>
2021-01-31 19:43:26 +0200

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+
+module.exports = {
+  entry: './src/insdex.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
