7b75320a8dac915e15e2a35c77cbd443d38f3185
vmwhoami <vmwhoami@gmail.com>
2021-02-01 17:31:13 +0200

    Add react linters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import "./styles/application.scss"
-import Nav from './components/Nav'
+import React from 'react';
+import './styles/application.scss';
+import Nav from './components/Nav';
+
 const App = () => (
   <div className="container-f">
     <Nav />
+
   </div>
-)
+);
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,54 +1,47 @@
-import React from 'react'
-import { Home, Portfolio, User, Envelope, Blog } from './Svgs'
+import React from 'react';
+import {
+  Home, Portfolio, User, Envelope, Blog,
+} from './Svgs';
 
+const Nav = () => (
+  <nav className="mynav">
 
+    <a className="mynav__link" href="">
+      <i className="mynav__icon">
+        <Home />
+      </i>
+      <span className="mynav__link__a">Home</span>
+    </a>
 
-class Nav extends React.Component {
-  constructor() {
-    super()
-  }
-  render() {
-    return (
-      <nav className="mynav">
+    <a className="mynav__link" href="">
+      <i className="mynav__icon">
+        <Portfolio />
+      </i>
+      <span className="mynav__link__a">Portfolio</span>
+    </a>
 
-        <a className="mynav__link" href="/">
-          <i className='mynav__icon'>
-            <Home />
-          </i>
-          <span className="mynav__link__a" >Home</span>
-        </a>
+    <a className="mynav__link" href="">
+      <i className="mynav__icon">
+        <User />
+      </i>
+      <span className="mynav__link__a">About-me</span>
+    </a>
 
-        <a className="mynav__link" href="">
-          <i className='mynav__icon'>
-            <Portfolio />
-          </i>
-          <span className="mynav__link__a" >Portfolio</span>
-        </a>
+    <a className="mynav__link" href="">
+      <i className="mynav__icon">
+        <Envelope />
+      </i>
+      <span className="mynav__link__a">Contact</span>
+    </a>
 
-        <a className="mynav__link" href="">
-          <i className='mynav__icon'>
-            <User />
-          </i>
-          <span className="mynav__link__a" >About-me</span>
-        </a>
+    <a className="mynav__link" href="">
+      <i className="mynav__icon">
+        <Blog />
+      </i>
+      <span className="mynav__link__a">Blog</span>
+    </a>
 
-        <a className="mynav__link" href="">
-          <i className='mynav__icon'>
-            <Envelope />
-          </i>
-          <span className="mynav__link__a" >Contact</span>
-        </a>
+  </nav>
+);
 
-        <a className="mynav__link" href="">
-          <i className='mynav__icon'>
-            <Blog />
-          </i>
-          <span className="mynav__link__a" >Blog</span>
-        </a>
-
-      </nav>
-    )
-  }
-}
-
-export default Nav
+export default Nav;
