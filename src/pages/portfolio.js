5c4f510c970631f1437e8cebe035062809158ab5
vmwhoami <vmwhoami@gmail.com>
2021-02-04 17:50:14 +0200

    Separate portfolio to different components

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -9,7 +9,7 @@ const Portfolio = () => {
 
   const getPortItems = async () => {
     const items = await axios.get(url)
-    console.log(items.data);
+
     setPortimes(items.data)
   }
   useEffect(() => {
@@ -21,25 +21,12 @@ const Portfolio = () => {
     <div className="container">
       <Heading white="My" color="Portfolio" bg="Work"
       />
-      <div class="grid-container">
+      <div className="grid-container">
         {portitems.map(({ id, title, body, subtitle, github_link, live_link, main_image, thumb_image }) => {
           return (
-
-
-            <article className="post_container  ">
-              <div className="post_thumb ">
-                <a className="d-block position-relative overflow-hidden link_container" >
-                  <img className="img_fluid" src={thumb_image} />
-                </a>
-              </div>
-              <div className="post_content">
-                <div className="entry_header">
-                  <h3> <a className="text-decoration-none" >{title}</a> </h3>
-                </div>
-                <div className="entry_content">
-                  <p>{body} </p>
-                </div>
-              </div>
+            <article key={id} className="post_container  ">
+              <PostThumb thumb_image={thumb_image} />
+              <PostContent title={title} body={body} />
             </article>
           )
         })}
@@ -48,6 +35,32 @@ const Portfolio = () => {
   );
 }
 
+const PostThumb = ({ thumb_image }) => {
+
+  return (
+    <div className="post_thumb ">
+      <a className="d-block position-relative overflow-hidden link_container" >
+        <img className="img_fluid" src={thumb_image} />
+      </a>
+    </div>
+  )
+
+}
+
+const PostContent = ({ title, body }) => {
+
+  return (
+    <div className="post_content">
+      <div className="entry_header">
+        <h3> <a className="text-decoration-none" >{title}</a> </h3>
+      </div>
+      <div className="entry_content">
+        <p>{body} </p>
+      </div>
+    </div>
+  )
+
+}
 export default Portfolio;
 
 
