3d5087a79fcc5544b3354fea08315035239f8e87
vmwhoami <vmwhoami@gmail.com>
2021-02-04 17:25:11 +0200

    add display grid to items

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -21,29 +21,29 @@ const Portfolio = () => {
     <div className="container">
       <Heading white="My" color="Portfolio" bg="Work"
       />
-      {portitems.map(({ id, title, body, subtitle, github_link, live_link, main_image, thumb_image }) => {
-        return (
-          <div className="row">
-            <div className="col-12 col-md-6 col-lg-4 col-xl-4 my-5">
-              <article className="post_container  ">
-                <div className="post_thumb ">
-                  <a className="d-block position-relative overflow-hidden link_container" >
-                    <img className="img_fluid" src={thumb_image} />
-                  </a>
+      <div class="grid-container">
+        {portitems.map(({ id, title, body, subtitle, github_link, live_link, main_image, thumb_image }) => {
+          return (
+
+
+            <article className="post_container  ">
+              <div className="post_thumb ">
+                <a className="d-block position-relative overflow-hidden link_container" >
+                  <img className="img_fluid" src={thumb_image} />
+                </a>
+              </div>
+              <div className="post_content">
+                <div className="entry_header">
+                  <h3> <a className="text-decoration-none" >{title}</a> </h3>
                 </div>
-                <div className="post_content">
-                  <div className="entry_header">
-                    <h3> <a className="text-decoration-none" >{title}</a> </h3>
-                  </div>
-                  <div className="entry_content">
-                    <p>{body} </p>
-                  </div>
+                <div className="entry_content">
+                  <p>{body} </p>
                 </div>
-              </article>
-            </div>
-          </div>
-        )
-      })}
+              </div>
+            </article>
+          )
+        })}
+      </div>
     </div>
   );
 }
@@ -51,66 +51,3 @@ const Portfolio = () => {
 export default Portfolio;
 
 
-
-//         <div className="post_content">
-//             <div className="entry_header">
-//                 <h3> <a clss="text-decoration-none" href="/portfolio/35">Mastering_Grid</a> </h3>
-//             </div>
-//             <div className="entry_content">
-//                 <p> Use semantic HTML tags.
-// Use CSS selectors corr...</p>
-//             </div>
-//         </div>
-//     </article>
-// </div>
-
-
-
-
-
-
-//   <div className="col-12 col-md-6 col-lg-4 col-xl-4 my-5">
-//     <article className="post_container  ">
-//         <div className="post_thumb ">
-//            <a className="d-block position-relative overflow-hidden link_container" href="/portfolio/34">
-//               <img className="img_fluid" src="https://raw.githubusercontent.com/vmwhoami/danceter/pre-development/app/assets/images/Screenshot_1.jpg">
-// </a>        </div>
-//         <div className="post_content">
-//             <div className="entry_header">
-//                 <h3> <a clss="text-decoration-none" href="/portfolio/34"> Capstone Project</a> </h3>
-//             </div>
-//             <div className="entry_content">
-//                 <p> In this project I built Dance opinion applicati...</p>
-//             </div>
-//         </div>
-//     </article>
-// </div>
-
-
-
-
-
-
-//   <div className="col-12 col-md-6 col-lg-4 col-xl-4 my-5">
-//     <article className="post_container  ">
-//         <div className="post_thumb ">
-//            <a className="d-block position-relative overflow-hidden link_container" href="/portfolio/36">
-//               <img className="img_fluid" src="https://raw.githubusercontent.com/vmwhoami/Trillo/work/img/Screenshot_Desktop.jpg">
-// </a>        </div>
-//         <div className="post_content">
-//             <div className="entry_header">
-//                 <h3> <a clss="text-decoration-none" href="/portfolio/36">Trillo</a> </h3>
-//             </div>
-//             <div className="entry_content">
-//                 <p> Built using CSS/SCSS flexbox  Media queries wer...</p>
-//             </div>
-//         </div>
-//     </article>
-// </div>
-
-
-
-
-
-//     </div>
-// </div>
