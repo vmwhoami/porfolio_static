ce6fedec2c6cef3437c386d09a343c13d8609b13
vmwhoami <vmwhoami@gmail.com>
2021-02-06 16:25:05 +0200

    add img tag

--- src/pages/portfolioItem.js
+++ src/pages/portfolioItem.js
@@ -13,6 +13,7 @@ const PortfolioItem = () => {
   const getPortItems = async () => {
     const item = await axios.get(url)
     setPortItem(item.data)
+
   }
   useEffect(() => {
     getPortItems()
@@ -21,10 +22,77 @@ const PortfolioItem = () => {
 
   return (
     <div className="container">
-      <Heading white="some page" color="me" bg="the page"
+      <Heading white={portitem.title} color="" bg="Portfolio"
       />
+
+      <div className="imgcontainer">
+        <img className="imgcontainer__img" src={portitem.thumb_image} />
+      </div>
+
+
     </div>
   );
 }
 
+
+
 export default PortfolioItem;
+
+
+{/* <div className="row mt-5">
+
+<div className="col-lg-6 col-md-12">
+  <img className="img-fluid" src="https://raw.githubusercontent.com/vmwhoami/Mastering_Grid/without-linter/img/full_desk_screenshot.jpg">
+</div>
+
+
+<div className=" col-12 col-lg-6  ">
+  <em className=" my-5 ">A simple website </em>
+  <p>Use semantic HTML tags.
+Use CSS selectors correctly.
+Use HTML elements box model (margin, padding, width, height).
+Use industry-standard tools (flexbox) to place elements on the page.
+Ability to create UIs adaptable to different screen sizes using media-queries.
+Apply front-end best practices.
+Ability to create UIs adjusted to given designs.
+Strong English written and verbal communication.
+Ability to communicate information effectively to technical and non-technical people.
+Ability to translate business requirements into software solutions.
+Ability to work well in a fast-paced environment under deadlines.
+Ability to perform tasks and complete projects with minimal supervision.</p>
+  <h2>Technologies used</h2>
+<hr className="white-line mr-5">
+    HTML 
+    CSS 
+    SCSS 
+     
+     
+     
+
+  
+<div className="links">
+
+<a target="_blank" className="links__container" href="https://github.com/vmwhoami/Mastering_Grid">
+<span className="links__text">Github</span>
+<i className="links__icon">
+<svg>
+  <use xlink:href="#icon-github"></use>
+</svg>
+</i>
+</a>
+<a target="_blank" className="links__container" href="https://nostalgic-kilby-78f387.netlify.app/">
+<span className="links__text">Live</span>
+<i class="links__icon">
+<svg>
+  <use xlink:href="#icon-earth"></use>
+</svg>
+</i>
+</a>
+
+
+
+
+</div>
+  
+</div>
+</div> */}
