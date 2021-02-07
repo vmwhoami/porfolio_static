d2368f7dd55b8b76b12186eaa5fb60d08e224cd6
vmwhoami <vmwhoami@gmail.com>
2021-02-07 17:00:42 +0200

    separate live links to separate logic

--- src/pages/portfolioItem.js
+++ src/pages/portfolioItem.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Heading from '../components/Heading';
+import { Github, Earth } from '../components/Svgs'
 import axios from 'axios'
 
 import { Link, useParams } from 'react-router-dom';
@@ -22,30 +23,44 @@ const PortfolioItem = () => {
   const { title, subtitle, body, main_image, thumb_image, github_link, live_link } = portitem
   return (
     <div className="container">
-      <Heading white={title} color="" bg="Portfolio"
+      <Heading white="" color={title} bg="Portfolio"
       />
       <div className="portItemCont">
-        <ImageOfItem thumb_image={thumb_image} />
+        <ImageOfItem thumb_image={main_image} />
         <div className="content">
-          <em className="content__em">A simple website </em>
-          <p>Use semantic HTML tags.
-          Use CSS selectors correctly.
-          Use HTML elements box model (margin, padding, width, height).
-          Use industry-standard tools (flexbox) to place elements on the page.
-          Ability to create UIs adaptable to different screen sizes using media-queries.
-          Apply front-end best practices.
-          Ability to create UIs adjusted to given designs.
-          Strong English written and verbal communication.
-          Ability to communicate information effectively to technical and non-technical people.
-          Ability to translate business requirements into software solutions.
-          Ability to work well in a fast-paced environment under deadlines.
-Ability to perform tasks and complete projects with minimal supervision.</p>
+          <em className="content__em">{subtitle} </em>
+          <p>{body}</p>
+          <h2>Technologies used</h2>
+          <hr className="white-line " />
+          <div className="links">
+
+
+            <LiveLinks github_link={github_link} Github={Github} />
+            <LiveLinks live_link={live_link} Earth={Earth} />
+            <a target="_blank" className="links__container" href={live_link}>
+              <span className="links__text">back to portfolios</span>
+              <i className="links__icon">
+              </i>
+            </a>
+          </div>
         </div>
 
       </div>
-    </div>
+    </div >
   );
 }
+
+const LiveLinks = ({ live_link, github_link, Earth, Github }) => {
+  return (
+    <a target="_blank" className="links__container" href={live_link ? live_link : github_link}>
+      <span className="links__text">{Earth ? "Live" : "Github"}</span>
+      <i className="links__icon">
+        {Earth ? <Earth /> : <Github />}
+      </i>
+    </a>
+  )
+}
+
 
 const ImageOfItem = ({ thumb_image }) => {
   return (
@@ -58,58 +73,3 @@ const ImageOfItem = ({ thumb_image }) => {
 
 export default PortfolioItem;
 
-
-{/* <div className="row mt-5">
-
- 
-
-<div className=" col-12 col-lg-6  ">
-  <em className=" my-5 ">A simple website </em>
-  <p>Use semantic HTML tags.
-Use CSS selectors correctly.
-Use HTML elements box model (margin, padding, width, height).
-Use industry-standard tools (flexbox) to place elements on the page.
-Ability to create UIs adaptable to different screen sizes using media-queries.
-Apply front-end best practices.
-Ability to create UIs adjusted to given designs.
-Strong English written and verbal communication.
-Ability to communicate information effectively to technical and non-technical people.
-Ability to translate business requirements into software solutions.
-Ability to work well in a fast-paced environment under deadlines.
-Ability to perform tasks and complete projects with minimal supervision.</p>
-  <h2>Technologies used</h2>
-<hr className="white-line mr-5">
-    HTML 
-    CSS 
-    SCSS 
-     
-     
-     
-
-  
-<div className="links">
-
-<a target="_blank" className="links__container" href="https://github.com/vmwhoami/Mastering_Grid">
-<span className="links__text">Github</span>
-<i className="links__icon">
-<svg>
-  <use xlink:href="#icon-github"></use>
-</svg>
-</i>
-</a>
-<a target="_blank" className="links__container" href="https://nostalgic-kilby-78f387.netlify.app/">
-<span className="links__text">Live</span>
-<i class="links__icon">
-<svg>
-  <use xlink:href="#icon-earth"></use>
-</svg>
-</i>
-</a>
-
-
-
-
-</div>
-  
-</div>
-</div> */}
